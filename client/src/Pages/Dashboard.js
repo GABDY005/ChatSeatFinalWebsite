51d49e066b1da150499cc99e9b5e62b67ea49dee
GABDY005 <gabdy005@mymail.unisa.edu.au>
2025-03-30 19:15:08 +1030

    Added the content of Dashboard page and added the layout using Bootstrap

---
+++ client/src/Pages/Dashboard.js
@@ -0,0 +1,70 @@
+
+
+
+
+function Dashboard() {
+    return(
+        <div className = "container mt-5 mb-5">
+            <h1 className="text-primary mb-4">Welcome to "Chat Seat"</h1>
+
+                    <p>
+                        In a bid to tackle social isolation, we would like to establish a series of chats seats acros Australia. The <strong>Chat Seats</strong> initiative aims to provide a safe and inviting place for people to talk. It will allow members of the community to <strong>“Connect through Conversation.”</strong> For this initiative to be successful we need people like yourselves to volunteer to be either Listeners and/or Coordinators.
+                    </p>
+
+                    <p>
+                        This website is also open to individuals who may have stopped to chat on one of our Chat Seats. If you are looking for when a particular Listener is scheduled to visit your Chat Seat, please sign in and go to the Calendar and find your venue and Listener.
+                    </p>
+
+
+                    <h2 className="mt-5 text-secondary">What do we mean by being a Listener or Coordinator?</h2>
+
+                    <h3 className="mt-4 text-info">Listeners</h3>
+
+                    <p>
+                        <strong>Listeners</strong> are individuals who volunteer their time to sit on a <strong>Chat Seat</strong> and listen to members of the community who may join them. Some of these members of the community may also be lonely and/or isolated. We are hoping to connect them back into their community through conversation.
+                    </p>
+
+                    <p>
+                        So, who would be a great volunteer? Someone who enjoys listening to other people and is interested in what is happening for them. Also, someone who has a couple of hours each week where they would like to sit at one of our Chat Seat locations. Does this sound like you?
+                    </p>
+
+                    <p>
+                        If you would like to volunteer as a Listener, then you will need to sign in and proceed to the page for Listeners. On this page you will find some helpful information, a booking calendar and a place where you can chat with other Listeners.
+                    </p>
+
+                    <h3 className="mt-4 text-info">Coordinators</h3>
+
+                    <p>
+                        <strong>Coordinators</strong> are individuals who are also volunteers and may also choose to be Listeners. They will help to establish a site and support the Listeners. So, who would be a good Coordinator? Someone who enjoys negotiating with potential venue “managers” and has good organisational skills.Coordinators are individuals who are also volunteers and may also choose to be Listeners. They will help to establish a site and support the Listeners. So, who would be a good Coordinator? Someone who enjoys negotiating with potential venue “managers” and has good organisational skills.
+                    </p>
+
+                    <p>
+                        If you would like to volunteer as a Coordinator, then you will need to sign in and proceed to the page for Coordinators. On this page you will find some helpful information, and a place where you can chat with other Coordinators.
+                    </p>
+
+                    <h2>Does this sound like something tat you would be interesting in doin?</h2>
+
+                    <p>
+                        If you would like to volunteer as a Coordinator, then you will need to sign in and proceed to the page for Coordinators. On this page you will find information about “Lessons learned” which describes some of the lessons we and others have learned when they established Chat Seats. There are also files of the graphics that have been used previously: banner, logo, name tags.
+                    </p>
+
+                    <h3 className="mt-5 text-secondary">How do you identify a Chat Seat</h3>
+
+                    <p>
+                        It will have a banner like the one below, at one of our Chat Seats locations.
+                    </p>
+
+                    <p>
+                        We hope you have the passion and the time to be part of this initiative.
+                    </p>
+
+                    <p>
+                        Thank you for taking the time to visit our website. We welcome any suggestions.
+                    </p>
+
+                    <p>Warm Regards,</p>
+                    <p><strong>Tricia and Noel</strong></p>
+        </div>
+    );
+    
+}
---
+++ client/src/Pages/Home.js
@@ -0,0 +1,13 @@
+function Home() {
+    return(
+        <div className="home-container">
+            <h1>Welcome to "Chat Seat"</h1>
+
+            <p>
+            In a bid to tackle social isolation, we would like to establish a series of chats seats acros Australia. The <strong>Chat Seats</strong> initiative aims to provide a safe and inviting place for people to talk. It will allow members of the community to <strong>“Connect through Conversation.”</strong> For this initiative to be successful we need people like yourselves to volunteer to be either Listeners and/or Coordinators.
+            </p>
+
+        </div>
+    );
+    
+}
